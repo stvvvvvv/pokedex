534595cffa05c5962a37f31b28c8070c916c3de3
stvvvvvv <stv5458@gmail.com>
2021-08-09 02:17:11 +0300

    New
    1. Start template

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="pokemon-card__wrapper">
+        <div class="pokemon-card__container">
+            <h2 class="pokemon-card__titile">PokemonCard Title</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PokemonCard'
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/Wrapper.vue
@@ -0,0 +1,15 @@
+<template lang="">
+ <PokemonCard />
+</template>
+
+<script>
+import PokemonCard from './PokemonCard'
+
+export default {
+  name: 'Wrapper',
+  components: { PokemonCard }
+}
+</script>
+
+<style lang="scss">
+</style>
