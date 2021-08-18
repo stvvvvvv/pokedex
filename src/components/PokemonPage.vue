3d9cb9859a588b4ad03486253259374cde2e329c
stvvvvvv <stv5458@gmail.com>
2021-08-18 15:36:36 +0300

    New

---
+++ src/components/PokemonPage.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="pokemon-page__wrapper">
+        <div class="pokemon-page__container">
+            <h1>{{ pokemonInfo }}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'PokemonPage',
+  props: {
+    pokemon_data: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
+  data () {
+    return {
+      pokemonInfo: {
+        type: Object,
+        default () {
+          return {}
+        }
+      },
+      pokemonUrl: 'https://pokeapi.co/api/v2/pokemon/'
+    }
+  },
+  methods: {
+    getPokemonID (pokemonId) {
+      pokemonId = this.pokemon_data.url.split('/')
+        .filter(function (part) { return !!part }).pop()
+      return pokemonId
+    }
+  },
+  mounted () {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon/')
+      .then(response => (this.pokemonInfo = response))
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
